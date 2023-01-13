3cb77309693443c28e741549992542d6415b7004
emin <eminyetim25@gmail.com>
2023-01-13 14:20:28 +0300

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+           Methotlar ornek = new Methotlar();
+           int sonuc = ornek.arttirVeTopla(ref a , ref b);
+           ornek.EkranaYazdir(Convert.ToString(sonuc));
+           ornek.EkranaYazdir(Convert.ToString(Topla(a,b)));
+
+        }
+    
+        private static int Topla(int a , int b)
+        {
+            return (a+b);
+        }
+    }
+
+    class Methotlar
+    {
+        public  void EkranaYazdir(string data)
+        {
+            System.Console.WriteLine("Sonuc :"+data);
+        }
+
+        public  int arttirVeTopla(ref int a , ref int b)
+        {
+            a+=1;
+            b+=1;
+            return (a + b);
+        }
+    }
+}
